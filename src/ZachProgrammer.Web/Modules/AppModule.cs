b490146d368bf8c9e2cbf48e90cdea2816b992a8
Bobby Johnson <bobby.johnson@gmail.com>
2011-08-20 22:02:38 -0700

    initial commit

---
+++ src/ZachProgrammer.Web/Modules/AppModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace ZachProgrammer.Web.Modules
+{
+    public class AppModule : NancyModule
+    {
+        public AppModule(): base()
+        {
+
+        }
+
+        public AppModule(string modulePath) : base(modulePath)
+        {
+
+        }
+
+    }
+}
---
+++ src/ZachProgrammer.Web/Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZachProgrammer.Web.Modules
+{
+    public class HomeModule : AppModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = x => View["index"];
+        }
+    }
+}
